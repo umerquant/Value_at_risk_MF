cf2924d37309bcd0f6538b9d7d0a373e280e10c1
Umer Shaikh <umerfshk133@gmail.com>
2022-01-13 19:05:31 +0530

    Update Hist_Var.py

--- Hist_Var.py
+++ Hist_Var.py
@@ -4,15 +4,15 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-sp_returns = np.loadtxt("ngen_axis_scmf.csv", delimiter = ",", usecols = 1)
-sp_returns.size
+mf_returns = np.loadtxt("ngen_axis_scmf.csv", delimiter = ",", usecols = 1)
+mf_returns.size
 
 I0 = 100000            #Investment position in the particular Asset /Index. 
 prob = 0.35
 
-np.percentile(sp_returns, prob * 100)
+np.percentile(mf_returns, prob * 100)
 
-var_h = np.percentile(sp_returns, prob * 100) * I0 #Historical Value at Risk 
+var_h = np.percentile(mf_returns, prob * 100) * I0 #Historical Value at Risk 
 
 print(var_h)
 
